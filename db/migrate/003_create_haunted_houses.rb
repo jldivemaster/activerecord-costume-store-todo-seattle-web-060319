a0d89519859e4ffb3ec1b4353cc1a5f687aafa44
Jason Leach <jlspursfan@gmail.com>
2019-06-14 21:14:14 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,8 +7,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :theme
       t.integer :price
       t.boolean :family_friendly?
-      t.timestamp :opening_date
-      t.timestamp :closing_time
+      t.datetime :opening_date
+      t.datetime :closing_time
       t.string :description
     end
   end
