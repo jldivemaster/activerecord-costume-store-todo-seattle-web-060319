bb03489de79a6cfd7e97a9364af65bd1aa2577aa
Jason Leach <jlspursfan@gmail.com>
2019-06-14 21:14:02 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,8 +7,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.integer :costume_inventory
       t.integer :num_employees
       t.boolean :in_business?
-      t.timestamp :opening_time
-      t.timestamp :closing_time
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 
